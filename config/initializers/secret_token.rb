cd34ebfe50d4cf8d3a4489a4481e3047546ec145
bpis <bpis@bpis-H61MLV2.(none)>
2012-10-23 13:31:05 +0530

    rails_apps_composer: initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-MoodyOn::Application.config.secret_token = '44828f2107808cfb35a32983b55f78b9afcfb99c52bcd15c2ca1aa904ddd37b830a490dc7e89ed742b8e24fe916f549d0026f7f4e2ebcc404c268c6e6a670688'
+MoodyOn::Application.config.secret_token = '52eff7fa8e034fc7cb6f79df026ebdb16653f06f703ed8529ef673fddff5d0b722a29d1e5f0ec311514aebdec98448316f510e0fe7e54ca9208fbafcda29941a'
